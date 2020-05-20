64504853125e8a0a4e6a1931a02bb35225ff62e2
Ed Andersen <mail@edandersen.com>
2020-05-20 15:36:54 +0100

    Enable hot reload of Razor in demo app

--- src/DotNetEd.CoreAdmin.DemoApp/Startup.cs
+++ src/DotNetEd.CoreAdmin.DemoApp/Startup.cs
@@ -1,26 +1,32 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using DotNetEd.CoreAdmin.DemoApp.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc.Razor.RuntimeCompilation;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 
 namespace DotNetEd.CoreAdmin
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
+            WebHostEnvironment = env;
         }
 
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment WebHostEnvironment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -29,7 +35,13 @@ namespace DotNetEd.CoreAdmin
 
             services.AddCoreAdmin();
 
-            services.AddControllersWithViews();
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
+
+            services.Configure<MvcRazorRuntimeCompilationOptions>(options =>
+            {
+                options.FileProviders.Add(new PhysicalFileProvider(Path.Combine(WebHostEnvironment.ContentRootPath, "..\\DotNetEd.CoreAdmin")));
+                options.AdditionalReferencePaths.Add(typeof(CoreAdminConfigurationExtensions).Assembly.Location);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
