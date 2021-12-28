0d5bf906e9562425b3f36aac0b4d0064d497b13a
Ed Andersen <mail@edandersen.com>
2021-12-28 15:55:24 +0000

    Show byte arrays on List view as images if possible

---
+++ src/DotNetEd.CoreAdmin.DemoAppDotNet6/Models/TestEntityWithImage.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DotNetEd.CoreAdmin.DemoAppDotNet6.Models
+{
+    public class TestEntityWithImage
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public byte[] Image { get; set; }
+    }
+}
---
+++ src/DotNetEd.CoreAdmin/ImageUtils.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetEd.CoreAdmin
+{
+    internal class ImageUtils
+    {
+        public static string WebBase64EncodeImageByteArrayOrNull(byte[] image)
+        {
+            var byteSignaturesAndContentTypes = new Dictionary<byte[], string>();
+            byteSignaturesAndContentTypes.Add(Encoding.ASCII.GetBytes("BM"), "image/bmp");
+            byteSignaturesAndContentTypes.Add(Encoding.ASCII.GetBytes("GIF"), "image/gif");
+            byteSignaturesAndContentTypes.Add(new byte[] { 137, 80, 78, 71 }, "image/png");
+            byteSignaturesAndContentTypes.Add(new byte[] { 73, 73, 42 }, "image/tiff");
+            byteSignaturesAndContentTypes.Add(new byte[] { 77, 77, 42 }, "image/tiff");
+            byteSignaturesAndContentTypes.Add(new byte[] { 255, 216, 255, 224 }, "image/jpeg");
+            byteSignaturesAndContentTypes.Add(new byte[] { 255, 216, 255, 225 }, "image/jpeg");
+
+            var matchingSig = byteSignaturesAndContentTypes
+                .Select(e => (KeyValuePair<byte[], string>?)e)
+                .FirstOrDefault(bs => bs.Value.Key.SequenceEqual(image.Take(bs.Value.Key.Length)));
+
+            if (matchingSig == null) return null;
+
+            var imageString = $"data:{matchingSig.Value.Value};base64," + Convert.ToBase64String(image);
+
+            return imageString;
+        }
+    }
+}
