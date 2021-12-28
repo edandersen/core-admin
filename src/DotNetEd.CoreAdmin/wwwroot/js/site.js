0d26625e9e2995e6fee87e81da453807bb06f5ad
Gaëtan Dezeiraud <gaetan@dezeiraud.com>
2021-12-28 19:30:57 +0100

    Fallback to the light theme if browser is not supported

--- src/DotNetEd.CoreAdmin/wwwroot/js/site.js
+++ src/DotNetEd.CoreAdmin/wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+$(document).ready(function () {
+    // If prefers-color-scheme is not supported, on older browser for example (IE).
+    // Use the light theme.
+    if (window.matchMedia("(prefers-color-scheme: dark)").media === "not all") {
+        $(".meta-css-dark").remove();
+    }
+});
