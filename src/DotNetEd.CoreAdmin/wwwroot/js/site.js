469b60320c538eaec7fea24a4ad1ff6acd16885a
DEZEIRAUD Gaëtan <gaetan@dezeiraud.com>
2022-01-24 14:41:18 +0100

    Sidebar is now toggable on desktop

--- src/DotNetEd.CoreAdmin/wwwroot/js/site.js
+++ src/DotNetEd.CoreAdmin/wwwroot/js/site.js
@@ -11,3 +11,50 @@ $(document).ready(function () {
         $(".meta-css-dark").remove();
     }
 });
+
+// Sidebar
+var mobileDeviceBreak = 768;
+var lastWindowWidth = $(window).width();
+function checkIfSidebarNeedToBeClosed() {
+    const windowWidth = $(window).width();
+
+    // Only update at the mobile UI transition
+    if (windowWidth <= mobileDeviceBreak && lastWindowWidth >= mobileDeviceBreak && !document.body.classList.contains("sb-sidenav-toggled")) {
+        document.body.classList.add("sb-sidenav-toggled");
+    } else if (windowWidth >= mobileDeviceBreak && lastWindowWidth <= mobileDeviceBreak && document.body.classList.contains("sb-sidenav-toggled")) {
+        if (localStorage.getItem("sb|sidebar-toggle") === "true") {
+            document.body.classList.add("sb-sidenav-toggled");
+        } else {
+            document.body.classList.remove("sb-sidenav-toggled");
+        }
+    }
+    lastWindowWidth = windowWidth;
+}
+
+$(document).ready(function () {
+    checkIfSidebarNeedToBeClosed();
+});
+
+$(window).resize(function () {
+    checkIfSidebarNeedToBeClosed();
+});
+
+window.addEventListener("DOMContentLoaded", event => {
+    // Toggle the side navigation
+    const sidebarToggle = document.body.querySelector("#sidebarToggle");
+    if (sidebarToggle) {
+        // Persist sidebar toggle between refreshes
+        if (localStorage.getItem("sb|sidebar-toggle") === "true" && $(window).width() > mobileDeviceBreak) {
+            document.body.classList.toggle("sb-sidenav-toggled");
+        }
+
+        // Add event listener
+        sidebarToggle.addEventListener("click", event => {
+            event.preventDefault();
+            document.body.classList.toggle("sb-sidenav-toggled");
+            if ($(window).width() > mobileDeviceBreak) {
+                localStorage.setItem("sb|sidebar-toggle", document.body.classList.contains("sb-sidenav-toggled"));
+            }
+        });
+    }
+});
