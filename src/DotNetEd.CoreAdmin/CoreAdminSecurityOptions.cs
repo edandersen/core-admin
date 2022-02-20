ada6223663cec1d8258ee2e1d511faf4dc629ddd
Cornel Hattingh <cornel.hattingh@outlook.com>
2022-02-20 21:30:42 +0200

    Allow Display of One To One Relationships in Grid and added some Grid Options

--- src/DotNetEd.CoreAdmin/CoreAdminSecurityOptions.cs
+++ src/DotNetEd.CoreAdmin/CoreAdminSecurityOptions.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NonFactors.Mvc.Grid;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -13,5 +14,14 @@ namespace DotNetEd.CoreAdmin
         public string CdnPath { get; set; }
         public Func<IServiceProvider, Task<bool>> CustomAuthorisationMethodWithServiceProvider { get; set; }
         public IEnumerable<Type> IgnoreEntityTypes { get; set; } = new List<Type>();
+        public Dictionary<Int32, string> PageSizes { get; set; }
+        public bool ShowPageSizes { get; set; }
+        public GridFilterMode FilterMode { get; set; }
+        public CoreAdminOptions()
+        {
+            PageSizes = new Dictionary<Int32, String> { { 0, "All" }, { 10, "10" }, { 20, "20" } };
+            ShowPageSizes = true;
+            FilterMode = GridFilterMode.Header;
+        }
     }
 }
---
+++ src/DotNetEd.CoreAdmin/DbContextExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetEd.CoreAdmin
+{
+    public static class DbContextExtensions
+    {
+        public static IQueryable<object> Set(this DbContext _context, Type t)
+        {
+           var methods =  _context.GetType().GetMethods();
+            foreach (var method in methods.Where(p=>p.Name == "Set"))
+            {
+                if(!method.GetParameters().Any())
+                return    (IQueryable<object>)method.MakeGenericMethod(t).Invoke(_context, null);
+            }
+            return (IQueryable<object>)_context.GetType().GetMethod("Set").MakeGenericMethod(t).Invoke(_context, null);
+        }
+    }
+}
